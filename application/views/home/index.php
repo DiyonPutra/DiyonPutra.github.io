f04754a49f0af5a7e1c9af05947ecefd1a0258e0
reaven75 <ardany.sunardi@gmail.com>
2023-01-19 11:29:58 +0700

    add file

---
+++ application/views/home/index.php
@@ -0,0 +1,14 @@
+<section class="section mt-5 pt-5">
+	<div class="section-body">
+		<div class="row">
+			<div class="col-md-6">
+				<h1>Sistem Pakar Penyakit Burung Murai</h1>
+				<p>Penyakit burung murai dapat menyebabkan gejala seperti kesulitan bernafas, berkurangnya aktivitas, dan perubahan warna kulit. Virus adalah penyebab utama penyakit ini, dan dapat menyebar melalui kontak langsung dengan burung yang terinfeksi atau melalui lingkungan yang terkontaminasi.</strong></p>
+				<a href="<?= base_url('/konsultasi'); ?>" class="btn btn-primary px-4">KONSULTASI</a>
+			</div>
+			<div class="col-md-6 d-none d-md-block d-lg-block">
+				<img src="<?= base_url('assets/img/murai.png'); ?>" alt="Konsultasi" class="img-fluid">
+			</div>
+		</div>
+	</div>
+</section>
